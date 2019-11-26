fff17b89f2a637d966975174b1b1960b595ad134
Pauras97 <paurasjadhav@gmail.com>
2019-11-26 13:18:34 -0700

    Relocation Mode

--- app/src/main/java/com/sourcey/relocator/Homepage.java
+++ app/src/main/java/com/sourcey/relocator/Homepage.java
@@ -1,13 +1,28 @@
 package com.sourcey.relocator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Homepage extends AppCompatActivity {
+
+    private Button rMode, vMode;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        rMode = (Button) findViewById(R.id.button2);
+
+        rMode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Homepage.this, RelocationMode.class));
+            }
+        });
     }
 }
