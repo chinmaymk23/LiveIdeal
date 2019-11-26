3681648796129816eac7c8aaddffa7c9607c39bf
Chinmay <chinmaymk23@gmail.com>
2019-11-25 23:38:55 -0700

    Added login functionality

--- app/src/main/java/com/sourcey/relocator/MainActivity.java
+++ app/src/main/java/com/sourcey/relocator/MainActivity.java
@@ -3,6 +3,7 @@ package com.sourcey.relocator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -13,9 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-        Intent intent = new Intent(this, LoginActivity.class);
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
         startActivity(intent);
     }
 
