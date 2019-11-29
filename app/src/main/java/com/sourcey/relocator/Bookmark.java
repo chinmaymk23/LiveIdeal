d2401ddd744e3b8cd435626c578c680beeaef04b
Pauras97 <paurasjadhav@gmail.com>
2019-11-28 22:10:49 -0700

    Add Bookmarks

--- app/src/main/java/com/sourcey/relocator/Bookmark.java
+++ app/src/main/java/com/sourcey/relocator/Bookmark.java
@@ -2,12 +2,33 @@ package com.sourcey.relocator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class Bookmark extends AppCompatActivity {
 
+    private ArrayList<String> pNames = new ArrayList<>();
+    private ArrayList<String> dButtons = new ArrayList<>();
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle  savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bookmark);
+        pNames.add("shwetak1");
+        pNames.add("shwetak2");
+        pNames.add("shwetak3");
+        pNames.add("shwetak4");
+        pNames.add("shwetak5");
+
+        initRecyclerView();
+    }
+
+    private void initRecyclerView(){
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, pNames, dButtons);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
