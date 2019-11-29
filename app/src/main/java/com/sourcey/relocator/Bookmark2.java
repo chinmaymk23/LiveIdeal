5d855cfe229d3c29ad83afc032beaffa2c219803
Pauras97 <paurasjadhav@gmail.com>
2019-11-28 23:14:15 -0700

    Add Bookmarks and relocation

--- app/src/main/java/com/sourcey/relocator/Bookmark2.java
+++ app/src/main/java/com/sourcey/relocator/Bookmark2.java
@@ -7,7 +7,7 @@ import android.support.v7.widget.RecyclerView;
 
 import java.util.ArrayList;
 
-public class Bookmark extends AppCompatActivity {
+public class Bookmark2 extends AppCompatActivity {
 
     private ArrayList<String> pNames = new ArrayList<>();
     private ArrayList<String> dButtons = new ArrayList<>();
@@ -15,12 +15,10 @@ public class Bookmark extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle  savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bookmark);
-        pNames.add("Grand Forks");
-        pNames.add("Cerrilios");
-        pNames.add("Houston");
-        pNames.add("Tampa");
-        pNames.add("Rockford");
+        setContentView(R.layout.activity_bookmark2);
+        pNames.add("Peru");
+        pNames.add("South Africa");
+        pNames.add("Amsterdam");
 
         initRecyclerView();
     }
